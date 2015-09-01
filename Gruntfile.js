e6e972f6afbcac617ccd28ad799d091249dc3ae2
Maxim Grishaev <grishaev@yandex-team.ru>
2015-09-01 13:55:30 +0300

    add: jade; sourcemaps

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,26 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jade');
 
     grunt.initConfig({
         browserify: {
-            options: {
-                debug: true,
-            },
             main: {
+                options: {
+                    browserifyOptions: {
+                        debug: true
+                    }
+                },
                 files: [
                     {src: ['./src/js/index.js'], dest: './dist/js/index.js'}
+                ]
+            }
+        },
+
+        jade: {
+            main: {
+                files: [
+                    {src: ['./src/index.jade'], dest: './dist/index.html'}
                 ]
             }
         },
@@ -22,16 +33,10 @@ module.exports = function (grunt) {
                     {expand:true, cwd: './src/css', src: '**', dest: './dist/css/'}
                 ]
             },
-
-            html: {
-                files: [
-                    {src: ['./src/index.html'], dest: './dist/index.html'}
-                ]
-            },
         },
     });
 
     grunt.registerTask('build', function () {
-        grunt.task.run('copy:css', 'copy:html', 'browserify')
+        grunt.task.run('copy:css', 'jade', 'browserify')
     });
 };
